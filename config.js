5f5beb6cc5ebaee67c23bbe6919a659c826b87b6
Jimwangom <jimymwangomi@gmail.com>
2025-01-22 10:03:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || '',
-  SUDO: process.env.SUDO || '',
+  SESSION_ID: process.env.SESSION_ID || 'XSTRO_09_81_64',
+  SUDO: process.env.SUDO || '255628470507',
   API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
   BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
   STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
   WARN_COUNT: process.env.WARN_COUNT || 3,
-  TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
+  TIME_ZONE: process.env.TIME_ZONE || 'Africa/Dodoma',
   VERSION: '1.3.0',
 };
 
